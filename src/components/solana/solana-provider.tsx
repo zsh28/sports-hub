5cf7888379b8b6638f0e6abee5abab61dd04e627
zsh28 <zeeshan8210@outlook.com>
2025-06-21 11:15:43 +0100

    fix wallets

--- src/components/solana/solana-provider.tsx
+++ src/components/solana/solana-provider.tsx
@@ -1,30 +1,46 @@
 'use client'
 
-import {WalletError} from '@solana/wallet-adapter-base'
-import {ConnectionProvider, WalletProvider,} from '@solana/wallet-adapter-react'
-import {WalletModalProvider} from '@solana/wallet-adapter-react-ui'
+import { WalletError } from '@solana/wallet-adapter-base'
+import {
+  ConnectionProvider,
+  WalletProvider
+} from '@solana/wallet-adapter-react'
+import {
+  WalletModalProvider
+} from '@solana/wallet-adapter-react-ui'
 import dynamic from 'next/dynamic'
-import {ReactNode, useCallback, useMemo} from 'react'
+import { ReactNode, useCallback, useMemo } from 'react'
+
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets'
 
 require('@solana/wallet-adapter-react-ui/styles.css')
 
-export const WalletButton = dynamic(async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton, {
-  ssr: false,
-})
+export const WalletButton = dynamic(
+  async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+  { ssr: false }
+)
 
 export function SolanaProvider({ children }: { children: ReactNode }) {
-  const cluster = useMemo(() => ({ endpoint: 'https://api.devnet.solana.com' }), [])
-  const endpoint = useMemo(() => cluster.endpoint, [cluster])
+  const endpoint = useMemo(() => 'https://api.devnet.solana.com', [])
+
+  const wallets = useMemo(
+    () => [
+    ],
+    []
+  )
+
   const onError = useCallback((error: WalletError) => {
     console.error(error)
   }, [])
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={[]} onError={onError} autoConnect={true}>
+      <WalletProvider wallets={wallets} onError={onError} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   )
 }
-
